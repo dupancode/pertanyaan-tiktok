5f1f581f2a7782da0f296812189180c1cd9a5001
Dupandes Milenium KD <dupandesdata@gmail.com>
2025-05-16 09:04:27 +0700

    Create index.php

---
+++ crud-testing/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CRUD Produk</title>
+  <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-gray-100 text-gray-800">
+
+  <div id="app"></div>
+
+  <!-- File Vue dipisah -->
+  <script type="module" src="./app.js"></script>
+</body>
+</html>
